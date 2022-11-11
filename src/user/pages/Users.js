6e0588bac41d0fae4fa090690a1485da29c7ae44
Rafa-angelino <rafaangelino22@gmail.com>
2022-11-11 01:20:14 -0300

    subindo

---
+++ src/user/pages/Users.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UserList from "../components/UserList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Rafael",
+      image:
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.infinance.com.br%2Fimages%2Fcomofunciona%2Fusuarios.png&imgrefurl=https%3A%2F%2Fwww.infinance.com.br%2Fusuarios.php&tbnid=vVPHtrLdGYDQuM&vet=12ahUKEwie-oyNoKX7AhXoNrkGHZhqAKcQMygJegUIARDQAQ..i&docid=t4CQJmDpyFWOGM&w=200&h=195&q=usu%C3%A1rios&ved=2ahUKEwie-oyNoKX7AhXoNrkGHZhqAKcQMygJegUIARDQAQ",
+    },
+  ];
+  return <UserList items={USERS} />;
+};
+
+export default Users;

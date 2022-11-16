ef64b72178ece3d235b1fdad14e0d6448a423e25
Rafa-angelino <rafaangelino22@gmail.com>
2022-11-15 22:34:23 -0300

    Adicionando validação de input

---
+++ src/shared/components/FormElements/Input.js
@@ -0,0 +1,78 @@
+import React, { useReducer } from "react";
+import "./Input.css";
+import { validate } from "../../util/validators";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+      case "TOUCH":
+        return {
+            ...state,
+            isTouched: true
+        }
+    default:
+      return state;
+  }
+};
+
+const Input = ({
+  label,
+  id,
+  element,
+  type,
+  placeholder,
+  rows,
+  validators,
+  errorText,
+}) => {
+
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  });
+
+  const changeHandler = (e) => {
+    dispatch({ type: "CHANGE", val: e.target.value, validators: validators });
+  };
+
+  const touchHandler = () => {
+    dispatch ({ type: 'TOUCH'})
+  }
+
+  const formType =
+    element === "input" ? (
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+        onBlur={touchHandler}
+      />
+    ) : (
+      <textarea 
+      id={id} 
+      rows={rows || 3} 
+      value={inputState.value} 
+      onBlur={touchHandler} />
+    );
+
+  return (
+    <div
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        "form-control--invalid"} `}
+    >
+      <label htmlFor={id}>{label}</label>
+      {formType}
+      {!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
